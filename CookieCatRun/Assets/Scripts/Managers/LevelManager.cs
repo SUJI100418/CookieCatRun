c467aa7a3470ae89d6e3bffabfce9094fb4426b9
suji1004 <suji1004@krafton.com>
2023-08-30 15:16:52 +0900

    UnityProjectInit

---
+++ CookieCatRun/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+스테이지쪽, 레벨
+맵 면체 (4면체, 8면체 등등)
+이동속도
+들어가는 타일 종류
+타일 종류 별 비율 (출력 확률도 노상관)
+스테이지쪽은 거리도 필요하네여
+그 스테이지 거리가 얼마나 될건디 
+ 아 그 누르는 정도에따라 점프량이 다르더라구여
+그거관련된 값이 좀 더 들가면되겠네여
+---------------
+캐릭터쪽
+점프량
+좌우 움직임 속도
+---------------
+타일 종류
+사이즈(가로는 면체 세로 사이즈)
+떨어지는 시간
+*/
+
+
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
